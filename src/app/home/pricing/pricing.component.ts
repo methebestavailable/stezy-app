4fc68fbc8763a60638d206ae3c96cb7305d271d2
methebestavailable <methebestavailable@gmail.com>
2019-01-18 19:21:34 +0530

    Home Ui changes

--- src/app/home/pricing/pricing.component.ts
+++ src/app/home/pricing/pricing.component.ts
@@ -58,7 +58,7 @@ export class PricingComponent implements OnInit {
            "Pay only for the nodes you <br/> provision ",
            "<span  class='li_bold'> Small Node  </span>  <br/> $199 / month <br/>  v1 CPU 1 GB Ram 8GB Storage",
            "<span  class='li_bold'> Medium Node </span>  <br/> $299 / month <br/>  v2 CPU 4 GB Ram 200GB Storage",
-           "<span  class='li_bold bor_none_last'> Large Node  </span>  <br/> $499 / month <br/>  v4 CPU 16 GB Ram 1TB Storage",
+           "<span  class='li_bold'> Large Node  </span>  <br/> $499 / month <br/>  v4 CPU 16 GB Ram 1TB Storage",
         ],
         height: false
 
@@ -88,7 +88,7 @@ export class PricingComponent implements OnInit {
      document.getElementsByClassName('li_bold')[0].setAttribute('_ngcontent-c1','')
      document.getElementsByClassName('li_bold')[1].setAttribute('_ngcontent-c1', '')
      document.getElementsByClassName('li_bold')[2].setAttribute('_ngcontent-c1', '')
-
+     document.getElementsByClassName('table_data')[1].lastElementChild.setAttribute('style', 'border:none')
 }, 5);
   }
 
