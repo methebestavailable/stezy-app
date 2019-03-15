0824d61dc504aadfeb5dc519cc59f0bf7525c683
methebestavailable <imadeveloper-me@gmail.com>
2019-03-15 18:52:03 +0530

    learn page changes

--- src/app/home/learn/learn.component.ts
+++ src/app/home/learn/learn.component.ts
@@ -36,6 +36,38 @@ export class LearnComponent implements OnInit {
     $('.side_menu').removeClass('showin');
 });
 
+$('.homeclick').on('click', function(e){
+  e.preventDefault();
+  $('#home').addClass('active show');
+  $('#getting').removeClass('active show');
+  $('#services').removeClass('active show');
+  $('#guide').removeClass('active show');
+  $('#api').removeClass('active show');
+  $('#other_faqs').removeClass('active show');
+  $('#support').removeClass('active show');
+});
+
+$('.gettingstarted').on('click', function(e){
+  e.preventDefault();
+  $('#home').removeClass('active show');
+  $('#getting').addClass('active show');
+  $('#services').removeClass('active show');
+  $('#guide').removeClass('active show');
+  $('#api').removeClass('active show');
+  $('#other_faqs').removeClass('active show');
+  $('#support').removeClass('active show');
+});
+
+$('.needsupport').on('click', function(e){
+  e.preventDefault();
+  $('#home').removeClass('active show');
+  $('#getting').removeClass('active show');
+  $('#services').removeClass('active show');
+  $('#guide').removeClass('active show');
+  $('#api').removeClass('active show');
+  $('#other_faqs').removeClass('active show');
+  $('#support').addClass('active show');
+});
     
 
     });
